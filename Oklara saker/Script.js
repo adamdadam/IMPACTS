a0f69e81f55db297916c1d79b7cb8faed0d4cdd3
hjalmaren <hjalle1994@hotmail.com>
2013-11-12 10:43:24 +0100

    Fixed
    Nu behöver man bara skriva vad som ska hända ifall man tappar focus

--- Oklara saker/Script.js
+++ Oklara saker/Script.js
@@ -1,45 +1,22 @@
-//Foucs Check
-var window_focus = true;
-$(window).focus(function() {
-    window_focus = true;
-})
-    .blur(function() {
-       window_focus = false;
-});
-
-$(document).one('click',function() {
-   setInterval(function() { $('body').append('has focus? ' + window_focus + '<br>'); }, 1000);
-});
-//
-
 $(function(){
-  var clickedBool=0;
-  var OMP=[0,0];
-  var NMP=[0,0];
-  $(document).keydown(function(){
-    clickedBool = 1;
+  var window_focus = true;
+
+  $(window).focus(function() {
+      window_focus = true;
+  })
+      .blur(function() {
+         window_focus = false;
   });
 
+  $(document).one('click',function() {
+     setInterval(function() {
+      if(window_focus == false){
+        console.log("false");
+      }else{
+        console.log("true");
+      }
+    }, 1000);
+  });
 
-
-  setInterval(function(){
-    OMP = NMP;
-    $(document).on("mousemove",function(e){
-        NMP = [e.pageX,e.pageY];
-    });
-
-    if(OMP[0] == NMP[0] && OMP[1] == NMP[1]){
-      clickedBool = 0;
-      console.log("mouse " + OMP[0] + " =? " + NMP[0]);
-      console.log("mouse " + OMP[1] + " =? " + NMP[1]);
-    }else{
-      clickedBool = 1;
-      console.log("one");
-    }
-
-    if(clickedBool == 0){
-      alert("You are afk!");
-    }
-    clickedBool = 0;
-  },10000);
 });
+
